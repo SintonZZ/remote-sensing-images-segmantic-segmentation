f546de73bb4472ce1e508076628dee8b5e4b1a45
Zhang.xd <18755150056@163.com>
2020-09-09 11:20:31 +0800

    Add files via upload

--- baseline-pytorch/train.py
+++ baseline-pytorch/train.py
@@ -4,12 +4,14 @@ from tqdm import tqdm
 import os
 from dataloader import train_loader, val_loader
 from model import unet
+from utils.metrics import SegmentationMetric
 # os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 
 num_classes = 8
+metric = SegmentationMetric(num_classes)
 
 def train(train_loader, model, criterion, optimizer):
-    train_loss_sum, train_acc_sum, n = 0.0, 0.0, 0
+    train_loss_sum, train_fwiou_sum, n = 0.0, 0.0, 0
     model.train()
 
     for input, target in tqdm(train_loader):
@@ -23,14 +25,21 @@ def train(train_loader, model, criterion, optimizer):
         loss.backward()
         optimizer.step()
 
+        for i in range(output.shape[0]):
+            pre = output[i, :, :, :].argmax(axis=0).cpu()
+            label = target[i, :, :].cpu()
+            metric.addBatch(pre, label)
+            FWIoU = metric.Frequency_Weighted_Intersection_over_Union()
+            train_fwiou_sum += FWIoU
+
         train_loss_sum += loss.sum().item()
-        train_acc_sum += (output.argmax(dim=1) == target).float().sum().item()
+        # train_acc_sum += (output.argmax(dim=1) == target).float().sum().item()
         n += target.shape[0]
 
-    return train_loss_sum / n, train_acc_sum / (n*256*256)
+    return train_loss_sum / n, train_fwiou_sum / n
 
 def validate(val_loader, model, criterion):
-    val_loss_sum, val_acc_sum, n = 0.0, 0.0, 0
+    val_loss_sum, val_fwiou_sum, n = 0.0, 0.0, 0
     model.eval()
     for input, target in tqdm(val_loader):
         input = input.cuda()
@@ -39,11 +48,18 @@ def validate(val_loader, model, criterion):
         output = model(input)
         loss = criterion(output, target.long())
 
+        for i in range(output.shape[0]):
+            pre = output[i, :, :, :].argmax(axis=0).cpu()
+            label = target[i, :, :].cpu()
+            metric.addBatch(pre, label)
+            FWIoU = metric.Frequency_Weighted_Intersection_over_Union()
+            val_fwiou_sum += FWIoU
+
         val_loss_sum += loss.sum().item()
-        val_acc_sum += (output.argmax(dim=1) == target).float().sum().item()
+        # val_acc_sum += (output.argmax(dim=1) == target).float().sum().item()
         n += target.shape[0]
 
-    return val_loss_sum / n, val_acc_sum / (n*256*256)
+    return val_loss_sum / n, val_fwiou_sum / n
 
 model = unet(num_classes=num_classes).cuda()
 criterion = nn.CrossEntropyLoss().cuda()
@@ -51,24 +67,14 @@ optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
 best_loss = 2020.0
 epochs = 1
-train_loss_l = []
-val_loss_l = []
-train_acc_l = []
-val_acc_l = []
 
 for epoch in range(epochs):
-    print('[Info]: Start epoch ', epoch)
-    train_loss, train_acc = train(train_loader, model, criterion, optimizer)
-    val_loss, val_acc = validate(val_loader, model, criterion)
+    train_loss, train_FWIoU = train(train_loader, model, criterion, optimizer)
+    val_loss, val_FWIoU = validate(val_loader, model, criterion)
     # scheduler.step()
 
-    train_loss_l.append(train_loss)
-    train_acc_l.append(train_acc)
-    val_loss_l.append(val_loss)
-    val_acc_l.append(val_acc)
-
-    print('Epoch %d: train loss %.4f, train acc %.3f, val_loss %.4f, val_acc %.3f'
-          % (epoch, train_loss, train_acc, val_loss, val_acc))
+    print('Epoch %d: train loss %.4f, train FWIoU %.3f, val_loss %.4f, val_FWIoU %.3f'
+          % (epoch, train_loss, train_FWIoU, val_loss, val_FWIoU))
     if val_loss < best_loss:
         best_loss = val_loss
         torch.save(model.state_dict(), './baseline.pt')
